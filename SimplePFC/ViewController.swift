c3336fb367b5a5c5ba52e8b1c5efdc9e54cef9ae
motto3532 <gropmor74@gmail.com>
2023-06-10 15:48:58 +0900

    Initial Commit

---
+++ SimplePFC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimplePFC
+//
+//  Created by Atto Rari on 2023/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
