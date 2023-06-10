79b931ccc2205f5ecd48e2b069afcac0cba25016
motto3532 <gropmor74@gmail.com>
2023-06-10 23:34:49 +0900

    containerViewを追加し、storyboardを切り分け。

---
+++ SimplePFC/MealViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MealViewController.swift
+//  SimplePFC
+//
+//  Created by Atto Rari on 2023/06/10.
+//
+
+import UIKit
+
+class MealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
