29b1f5b32f6b56300f7d4442a4a7195b982c0dfb
motto3532 <gropmor74@gmail.com>
2023-06-16 22:46:21 +0900

    食事内容を追加する画面を作成

---
+++ SimplePFC/AddMealViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddMealViewController.swift
+//  SimplePFC
+//
+//  Created by Atto Rari on 2023/06/16.
+//
+
+import UIKit
+
+class AddMealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
