860cfddf22039a43cf02b327e323e03ea419f2fb
motto3532 <gropmor74@gmail.com>
2023-06-17 21:49:24 +0900

    キーボードを閉じる処理と入力値を格納する処理

--- SimplePFC/AddMealViewController.swift
+++ SimplePFC/AddMealViewController.swift
@@ -7,9 +7,61 @@
 
 import UIKit
 
-class AddMealViewController: UIViewController {
+class AddMealViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var foodNameTextField: UITextField!
+    @IBOutlet weak var proteinTextField: UITextField!
+    @IBOutlet weak var fatTextField: UITextField!
+    @IBOutlet weak var carbohydrateTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        foodNameTextField.delegate = self
+        proteinTextField.delegate = self
+        fatTextField.delegate = self
+        carbohydrateTextField.delegate = self
+        
+        //キーボードを数字のみにする
+        proteinTextField.keyboardType = UIKeyboardType.numberPad
+        fatTextField.keyboardType = UIKeyboardType.numberPad
+        carbohydrateTextField.keyboardType = UIKeyboardType.numberPad
+        
     }
+    
+    //食事内容を格納する変数
+    var meal: (Name: String, protein: Int, fat: Int, carbohydrate: Int) = ("", 0, 0, 0)
+    
+    @IBAction func addButtonAction(_ sender: Any) {
+        //食事内容をtextField毎に格納
+        //食事名
+        if let foodName = foodNameTextField.text {
+            meal.Name = foodName
+        }
+        //タンパク質
+        if let protein = proteinTextField.text {
+            meal.protein = Int(protein) ?? 0
+        }
+        //脂質
+        if let fat = fatTextField.text {
+            meal.fat = Int(fat) ?? 0
+        }
+        //炭水化物
+        if let carbohydrate = carbohydrateTextField.text {
+            meal.carbohydrate = Int(carbohydrate) ?? 0
+        }
+        
+        //contentViewControllerに渡す処理
+        print("入力された食事内容は\(meal)")
+    }
+    
+    //文字キーボードを閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
 }
