01522c7d81de7cf83dd800be423f96b17d365b09
motto3532 <gropmor74@gmail.com>
2023-08-14 23:04:41 +0900

    カレンダーに食事内容が登録されている日付には色をつけるように実装

--- SimplePFC/Calendar/CalendarViewController.swift
+++ SimplePFC/Calendar/CalendarViewController.swift
@@ -17,6 +17,9 @@ final class CalendarViewController: UIViewController{
         }
     }
     
+    private var eventDates: [String] = []
+    private let dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //rootでこの画面きたらすぐmealsへ移動
@@ -25,6 +28,9 @@ final class CalendarViewController: UIViewController{
         calendar.calendarHeaderView.backgroundColor = .white
         calendar.calendarWeekdayView.backgroundColor = .white
         calendar.backgroundColor = .white
+        calendar.appearance.headerTitleColor = .gray
+        calendar.appearance.weekdayTextColor = .gray
+        calendar.appearance.todayColor = .lightGray
         calendar.appearance.headerDateFormat = "YYYY年MM月"
         calendar.calendarWeekdayView.weekdayLabels[0].text = "日"
         calendar.calendarWeekdayView.weekdayLabels[1].text = "月"
@@ -34,16 +40,53 @@ final class CalendarViewController: UIViewController{
         calendar.calendarWeekdayView.weekdayLabels[5].text = "金"
         calendar.calendarWeekdayView.weekdayLabels[6].text = "土"
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.eventDates = []
+        
+        self.dateFormatter.dateFormat = "yyyy/MM/dd"
+        let realmRegistedData = MealRealm.shared.getMealsData()
+        for data in realmRegistedData {
+            let dateStr = self.dateFormatter.string(from: data.date)
+            if !self.eventDates.contains(dateStr) {
+                self.eventDates.append(dateStr)
+            }
+        }
+        
+        self.calendar.reloadData()
+    }
 }
 
 extension CalendarViewController: FSCalendarDelegate {
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         //dateはDate型だから、絶対時間(日本より9時間遅い)が格納されてる
-        //print(date)
         Router.shared.showMeals(from: self, date: date)
+        self.calendar.deselect(date)
     }
 }
 
 extension CalendarViewController: FSCalendarDataSource {
     
+}
+
+extension CalendarViewController: FSCalendarDelegateAppearance {
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, fillDefaultColorFor date: Date) -> UIColor? {
+        let dateStr = dateFormatter.string(from: date)
+        if self.eventDates.contains(dateStr) {
+            return UIColor.systemMint
+        } else {
+            return nil
+        }
+    }
+    
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
+        let dateStr = dateFormatter.string(from: date)
+        if self.eventDates.contains(dateStr) {
+            return UIColor.white
+        } else {
+            return nil
+        }
+    }
 }
